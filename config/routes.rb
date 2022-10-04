01a07e90f29ca7ab0cd3e6d63c51b2228d695ee4
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-10-04 18:55:50 +0200

    Init: Pages needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/affaires', to: 'pages#affaires' # nouveau nom url : quoi prendre ?
+  get '/programme', to: 'pages#programme'
+  get '/transports', to: 'pages#transports' # nouveau nom url : comment s'y rendre?
+  get '/repas', to: 'pages#repas' # nouveau nom url : ?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
