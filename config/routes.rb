e19456c34ea2090a52abf6c4e10c8a6812b7ab1f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-10-05 08:51:02 +0200

    Init: pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/transports', to: 'pages#transports'
+  get '/repas', to: 'pages#repas'
+  get '/activites', to: 'pages#activites'
+  get '/affaires', to: 'pages#affaires'
+  get '/lieu', to: 'pages#lieu'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
