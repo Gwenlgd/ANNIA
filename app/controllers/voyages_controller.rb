d5b726f86e1c67562c7c57648947228ef830fe26
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-10-18 14:00:22 +0200

    Init: Controller for Voyages

--- app/controllers/voyages_controller.rb
+++ app/controllers/voyages_controller.rb
@@ -6,13 +6,28 @@ class VoyagesController < ApplicationController
   end
 
   def show
-    @voyage = Voyage.new
     @voyage.user = current_user
   end
 
+  def new
+    @voyage = Voyage.new
+  end
 
+  def create
+    @voyage = Voyage.new(voyage_params)
+    @voyage.user_id = current_user.id
+    if @voyage.save
+      redirect_to voyages_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
+
+  def voyage_params
+    params.require(:voyage).permit(:category, :ville, :rendezvous, :place, :heure_arrivee, :date_arrivee, :heure_depart, :date_depart)
+  end
 
   def set_voyage
     @voyage = Voyage.find(params[:id])
