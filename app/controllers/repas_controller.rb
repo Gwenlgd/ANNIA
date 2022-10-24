c2b5d5d76077508039b77c54aa8bb154b4e9c919
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-10-24 09:43:45 +0200

    Init: Controller repas

--- app/controllers/repas_controller.rb
+++ app/controllers/repas_controller.rb
@@ -1,2 +1,49 @@
 class RepasController < ApplicationController
+  before_action :set_repa, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @repas = Repa.all
+  end
+
+  def show
+  end
+
+  def new
+    @repa = Repa.new
+  end
+
+  def create
+    @repa = Repa.new(repa_params)
+    if @repa.save
+      redirect_to repas_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @repa.update(repa_params)
+      redirect_to repa_path(@repa)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @repa.destroy
+    redirect_to repas_path, status: :see_other
+  end
+
+  private
+
+  def repa_params
+    params.require(:repa).permit(:items, :allergies, :quantities, :drinks, :user_id)
+  end
+
+  def set_repa
+    @repa = Repa.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,28 @@ require "json"
 
 puts "Cleaning database..."
 Voyage.destroy_all
+Repa.destroy_all
 User.destroy_all
 
 puts "Creating users..."
 user1 = User.create!(first_name: "Gwen", last_name: "Leg", email: "gwenl@test.com", password: "paradise")
 user2 = User.create!(first_name: "Mathilde", last_name: "Leg", email: "mathildel@test.com", password: "paradise")
+user3 = User.create!(first_name: "Claire", last_name: "Leg", email: "clairel@test.com", password: "paradise")
+user4 = User.create!(first_name: "Alix", last_name: "Leg", email: "alixl@test.com", password: "paradise")
+user5 = User.create!(first_name: "Jules", last_name: "Leg", email: "jules@test.com", password: "paradise")
+user6 = User.create!(first_name: "Laure", last_name: "Leg", email: "laure@test.com", password: "paradise")
+user7 = User.create!(first_name: "Margaux", last_name: "Leg", email: "margaux@test.com", password: "paradise")
+user8 = User.create!(first_name: "Gaïa", last_name: "Leg", email: "Gaia@test.com", password: "paradise")
 
 puts "Creating voyages..."
-# voyage_gwen = Voyage.create!(category: "Voiture", ville: "Paris", rendezvous: "Paris 15", place: "3", heure_arrivee: "09:26:00" , date_arrivee: "2022-10-20" , heure_depart: "09:26:00" , date_depart: "2022-10-22",user: user1)
+voyage_gwen = Voyage.create!(category: "Voiture", ville: "Paris", rendezvous: "Paris 15", place: "3", heure_arrivee: "09:26:00" , date_arrivee: "2022-10-20" , heure_depart: "09:26:00" , date_depart: "2022-10-22",user: user1)
 
-# voyage_gwen2 = Voyage.create!(category: "Train", ville: "Paris", rendezvous: "Paris 15", place: "2", heure_arrivee: "09:26:00" , date_arrivee: "2022-10-20" , heure_depart: "09:26:00" , date_depart: "2022-10-22",user: user1)
+voyage_gwen2 = Voyage.create!(category: "Train", ville: "Paris", rendezvous: "Paris 15", place: "2", heure_arrivee: "09:26:00" , date_arrivee: "2022-10-20" , heure_depart: "09:26:00" , date_depart: "2022-10-22",user: user1)
 
-voyage_gwen2 = Voyage.create!(category: "Train", ville: "Nice", rendezvous: "Paris 15", place: "2", heure_arrivee: "09:26:00" , date_arrivee: "2022-10-20" , heure_depart: "09:26:00" , date_depart: "2022-10-22",user: user2)
+voyage_mathilde = Voyage.create!(category: "Train", ville: "Nice", rendezvous: "Paris 15", place: "2", heure_arrivee: "09:26:00" , date_arrivee: "2022-10-20" , heure_depart: "09:26:00" , date_depart: "2022-10-22",user: user2)
+
+puts "Creating repas..."
+
+repas1 = Repa.create!(items: "Salade de riz", allergies: "None", quantities: "1", drinks: "Vin", user: user1)
+repas2 = Repa.create!(items: "Quiche", allergies: "Vegan", quantities: "2", drinks: "Vin", user: user2)
+repas2 = Repa.create!(items: "Chips", allergies: "Gluten", quantities: "5", drinks: "Vin", user: user3)
