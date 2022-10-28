09daa59e3cfdf33102f85ce539c2e767d2aeef89
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-10-28 15:36:24 +0200

    Modif: Change on Repas index's page

--- app/controllers/lits_controller.rb
+++ app/controllers/lits_controller.rb
@@ -1,2 +1,20 @@
 class LitsController < ApplicationController
+  before_action :set_lit, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lits = Lit.all
+  end
+
+  def edit
+  end
+
+  private
+
+  def voyage_params
+    params.require(:voyage).permit(:category, :ville, :place, :rendezvous, :date_depart, :heure_depart, :date_arrivee, :heure_arrivee, :user_id)
+  end
+
+  def set_voyage
+    @voyage = Voyage.find(params[:id])
+  end
 end
