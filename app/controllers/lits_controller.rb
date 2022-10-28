e50bfe08db6f1ea8087d1bbaa831f3f6ec4bd298
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-10-28 15:46:23 +0200

    Init: Controller Lits

--- app/controllers/lits_controller.rb
+++ app/controllers/lits_controller.rb
@@ -1,20 +1,51 @@
 class LitsController < ApplicationController
   before_action :set_lit, only: [:show, :edit, :update, :destroy]
 
+  # /lits
   def index
     @lits = Lit.all
   end
 
+  # /lits/new
+
+  def new
+    @lit = Lit.new
+  end
+
+  def create
+    @lit = Lit.new(lit_params)
+    if @lit.save
+      redirect_to lits_path, notice: "Item was saved successfully."
+    else
+      flash[:error] = "Error creating item. Please try again."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # /lits/:id/edit
   def edit
+  end
+
+  def update
+    if @lit.update(lit_params)
+      redirect_to lit_path(@lit)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @lit.destroy
+    redirect_to lits_path, status: :see_other
   end
 
   private
 
-  def voyage_params
-    params.require(:voyage).permit(:category, :ville, :place, :rendezvous, :date_depart, :heure_depart, :date_arrivee, :heure_arrivee, :user_id)
+  def lit_params
+    params.require(:lit).permit(:nombre_place)
   end
 
-  def set_voyage
-    @voyage = Voyage.find(params[:id])
+  def set_lit
+    @lit = Lit.find(params[:id])
   end
 end
